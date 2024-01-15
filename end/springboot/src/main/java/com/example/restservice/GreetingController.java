8ee29143214cc5d3eeac9588cf86097a3c9b0da7
lzqdiy <lzqdiy@163.com>
2024-01-15 17:31:56 +0900

    add yaml file

--- end/springboot/src/main/java/com/example/restservice/GreetingController.java
+++ end/springboot/src/main/java/com/example/restservice/GreetingController.java
@@ -2,10 +2,13 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restservice.domain.User;
 
 @RestController
 @CrossOrigin
@@ -14,9 +17,12 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@Autowired
+	private User user;
+
 	@GetMapping("/")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		// return new Greeting(counter.incrementAndGet(), String.format(template, name));
-		return "Hello SpringBoot!";
+		return user.getUsername()+":Hello SpringBoot!";
 	}
 }
---
+++ end/springboot/src/main/java/com/example/restservice/domain/User.java
@@ -0,0 +1,26 @@
+package com.example.restservice.domain;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "user")
+public class User {
+    private int uid;
+    private String username;
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getUid(){
+        return this.uid;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+}
